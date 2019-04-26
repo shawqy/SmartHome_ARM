0dc3790f154ee026891936fe71c5166fa36f1436
tarek khaled <tarekt820@gmail.com>
2019-04-26 20:59:17 +0200

    implement sendByte function

--- uart.c
+++ uart.c
@@ -2,10 +2,6 @@
 #include "uart.h"
 
 
-
-
-
-
 /*This global array can only be accessed from this file*/
 /*This array holds the different UARTs modules for the tm4c123gh6pm controller*/
 /*All these addresses are pre-defined in the mic_config.h*/
@@ -22,3 +18,14 @@ static uint32_t USART_baseAddresses[]=
 	UART7_R_BASE_ADDRESSE
 
 };
+
+
+/* Send Byte From UART */
+void UART_sendByte(UART_Number uNumber,const uint8_t jOneChar) {
+
+	while((*((volatile uint32_t *)((USART_baseAddresses[uNumber]+UART_FLAG_R_OFFSET))))
+	& 0x00000020 != 0x00000020) {};
+
+	(*((volatile uint32_t *)((USART_baseAddresses[uNumber]+UART_DATA_R_OFFSET)))) = jOneChar ;
+
+}
