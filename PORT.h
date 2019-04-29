57eedc2338130de3bd83d7e4d8e1f19e0a648058
zyad mohamed <zezo19975@yahoo.com>
2019-04-29 22:29:28 +0200

    Create PORT.h

---
+++ PORT.h
@@ -0,0 +1,51 @@
+#ifndef PORT_H
+#define PORT_H
+
+#include "micro_config.h"
+#include "macros.h"
+#include "std_types.h"
+
+typedef enum
+{
+
+PORT_A,PORT_B,PORT_C,PORT_D,PORT_E,PORT_F
+
+
+}Port;
+
+
+typedef enum
+{
+
+PORT_PIN_IN,PORT_PIN_OUT
+
+
+}Port_PinDirectionType;
+
+
+// uint8 one byte usingned integer
+
+void Port_Init( Port port_index);
+/*Initialize port based on selected port_index (0 to
+5) by enabling the clock, unlocking the port, and
+making the selected mode digital*/
+
+void Port_SetPinDirection(Port port_index, uint8 pins_mask , Port_PinDirectionType I_O );
+/*Change the direction of the selected pins by
+pins_mask in the port selected by port_index
+I_O indecated if the pin is input or output 1 for output
+0 for input **Not so sure check datasheet :"D*/
+
+void Port_SetPinPullUp(Port port_index, uint8 pins_mask, uint8 enable);
+/*If enable is 1, the selected pins by pins_mask in
+the port selected by port_index will be
+connected to internal pull-up resistor.
+If enable is 0, the selected pins by pins_mask in
+the port selected by port_index will be not be
+connected to internal pull-up resistor.*/
+
+void Port_SetPinPullDown(Port port_index, uint8 pins_mask, uint8 enable);
+/*zae ele fo2 bs pull down bdl pull up !*/
+
+
+#endif
