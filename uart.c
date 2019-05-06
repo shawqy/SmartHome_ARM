1537db7d4ea345e7ac977f0d82dcedf0bb5c7744
zyad mohamed <zezo19975@yahoo.com>
2019-05-06 06:31:49 +0200

    Update uart.c

--- uart.c
+++ uart.c
@@ -111,7 +111,7 @@ void UART_sendByte(UART_Number uNumber, const uint8_t jOneChar)
 {
 
 	// 0x00000020 --> TXFF :: Transmitter Full Bit number 5
-	while (!(*((volatile uint32_t *)((USART_baseAddresses[uNumber] + UART_FLAG_R_OFFSET)))) & 0x00000020);
+	while (((*((volatile uint32_t *)((USART_baseAddresses[uNumber] + UART_FLAG_R_OFFSET)))) & 0x00000020)!= 0);
 	
 
 	(*((volatile uint32_t *)((USART_baseAddresses[uNumber] + UART_DATA_R_OFFSET)))) = jOneChar;
@@ -154,6 +154,9 @@ void UART_receiveString(UART_Number uNumber , uint8_t* Word){
 
 void UART_init(const UART_ConfigureStruct *configure_pointer)
 {
+	
+	
+	
 	uint8_t uart_num, word_wdth, prt_sel, f_en, stp, prt_en;
 
 	word_wdth = configure_pointer->UWL;
@@ -170,6 +173,35 @@ void UART_init(const UART_ConfigureStruct *configure_pointer)
 
 	prt_en = configure_pointer->UPE;
 	prt_en = (prt_en << 1);
+	
+	
+	
+	
+	
+	
+	
+	/*
+SYSCTL_RCGCUART_R |= SYSCTL_RCGCUART_R0;
+	SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R0;
+	
+	UART0_CTL_R &= ~UART_CTL_UARTEN;
+	UART0_IBRD_R = 520;    
+  UART0_FBRD_R = 53;
+	UART0_LCRH_R = ( word_wdth | prt_sel | stp | prt_en| f_en);
+	UART0_CTL_R |= (UART_CTL_RXE | UART_CTL_TXE | UART_CTL_UARTEN);
+	
+	GPIO_PORTA_AFSEL_R |= GPIO_PA10_M;
+	GPIO_PORTA_PCTL_R = (GPIO_PORTA_PCTL_R&0xFFFFFF00) | (GPIO_PCTL_PA1_U0TX | GPIO_PCTL_PA0_U0RX);
+	GPIO_PORTA_DEN_R |= GPIO_PA10_M;*/
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 	//enable system clk
 	SYSCTL_RCGCUART_R |= SYSCTL_RCGCUART_R0;
@@ -177,78 +209,118 @@ void UART_init(const UART_ConfigureStruct *configure_pointer)
 	//enable GPIO clk
 	SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R0;
 
+
+
 	//determine the the UART Number to know which TX and RX we will use
 	uart_num = configure_pointer->UN;
 
-	switch (uart_num)
-	{
-	case UART_0:
-		GPIO_PORTA_AFSEL_R |= GPIO_PA10_M;
-		GPIO_PORTA_DEN_R |= GPIO_PA10_M;
-		GPIO_PORTA_PCTL_R |= 0x11;
-		break;
 
-	case UART_1:
-		GPIO_PORTB_AFSEL_R |= GPIO_PB10_M;
-		GPIO_PORTB_DEN_R |= GPIO_PB10_M;
-		GPIO_PORTB_PCTL_R |= 0x11;
-		break;
 
-	case UART_2:
-		GPIO_PORTD_AFSEL_R |= GPIO_PD76_M;
-		GPIO_PORTD_DEN_R |= GPIO_PD76_M;
-		GPIO_PORTD_PCTL_R |= 0x11;
-		break;
+		//enable system clk
+		SYSCTL_RCGCUART_R |= (1 << uart_num);
 
-	case UART_3:
-		GPIO_PORTC_AFSEL_R |= GPIO_PC76_M;
-		GPIO_PORTC_DEN_R |= GPIO_PC76_M;
-		GPIO_PORTC_PCTL_R |= 0x11;
-		break;
-
-	case UART_4:
-		GPIO_PORTC_AFSEL_R |= GPIO_PC54_M;
-		GPIO_PORTC_DEN_R |= GPIO_PC54_M;
-		GPIO_PORTC_PCTL_R |= 0x11;
-		break;
-
-	case UART_5:
-		GPIO_PORTE_AFSEL_R |= GPIO_PE54_M;
-		GPIO_PORTE_DEN_R |= GPIO_PE54_M;
-		GPIO_PORTE_PCTL_R |= 0x11;
-		break;
-
-	case UART_6:
-		GPIO_PORTD_AFSEL_R |= GPIO_PD54_M;
-		GPIO_PORTD_DEN_R |= GPIO_PD54_M;
-		GPIO_PORTD_PCTL_R |= 0x11;
-		break;
-
-	case UART_7:
-		GPIO_PORTE_AFSEL_R |= GPIO_PE10_M;
-		GPIO_PORTE_DEN_R |= GPIO_PE10_M;
-		GPIO_PORTE_PCTL_R |= 0x11;
-		break;
-	}
 
 	//disable the UART to set it
-	(*((volatile uint32_t *)((USART_baseAddresses[uart_num] + UART_CTL_R_OFFSET)))) = ~UART_CTL_UARTEN;
+	(*((volatile uint32_t *)((USART_baseAddresses[uart_num] + UART_CTL_R_OFFSET)))) &= ~UART_CTL_UARTEN;
 
 	//adjust the Baud Rate
 	(*((volatile uint32_t *)((USART_baseAddresses[uart_num] + UART0_IBRD_R_OFFSET)))) = 520;
 	(*((volatile uint32_t *)((USART_baseAddresses[uart_num] + UART0_FBRD_R_OFFSET)))) = 53;
 
 	//adjust the UARTLCRH
-	(*((volatile uint32_t *)((USART_baseAddresses[uart_num] + UART_CRH_R_OFFSET)))) |= word_wdth;
-	(*((volatile uint32_t *)((USART_baseAddresses[uart_num] + UART_CRH_R_OFFSET)))) |= prt_sel;
-	(*((volatile uint32_t *)((USART_baseAddresses[uart_num] + UART_CRH_R_OFFSET)))) |= stp;
-	(*((volatile uint32_t *)((USART_baseAddresses[uart_num] + UART_CRH_R_OFFSET)))) |= prt_en;
+	(*((volatile uint32_t *)((USART_baseAddresses[uart_num] + UART_CRH_R_OFFSET)))) |= ( word_wdth | prt_sel | stp | prt_en| f_en);
+
 
 	//adjuct UARTCC to internal clock
 	(*((volatile uint32_t *)((USART_baseAddresses[uart_num] + UART_CC_R_OFFSET)))) = 0;
 	
 	//enable the UART to set it
-	(*((volatile uint32_t *)((USART_baseAddresses[uart_num] + UART_CTL_R_OFFSET)))) |= UART_CTL_UARTEN;
+	(*((volatile uint32_t *)((USART_baseAddresses[uart_num] + UART_CTL_R_OFFSET)))) |= (UART_CTL_RXE | UART_CTL_TXE | UART_CTL_UARTEN);
+
+
+
+
+
+
+
+
+
+	switch (uart_num)
+	{
+	case UART_0:
+
+
+		//enable GPIO clk
+		SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R0;
+		GPIO_PORTA_AFSEL_R |= GPIO_PA10_M;
+		GPIO_PORTA_PCTL_R |= (GPIO_PORTA_PCTL_R&0xFFFFFF00) | (GPIO_PCTL_PA1_U0TX | GPIO_PCTL_PA0_U0RX);;
+		GPIO_PORTA_DEN_R |= GPIO_PA10_M;
+		break;
+
+	case UART_1:
+
+		//enable GPIO clk
+		SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R1;
+		GPIO_PORTB_AFSEL_R |= GPIO_PB10_M;
+		GPIO_PORTB_DEN_R |= GPIO_PB10_M;
+		GPIO_PORTB_PCTL_R |= 0x11;
+		break;
+
+	case UART_2:
+
+		//enable GPIO clk
+		SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R3;
+		GPIO_PORTD_AFSEL_R |= GPIO_PD76_M;
+		GPIO_PORTD_DEN_R |= GPIO_PD76_M;
+		GPIO_PORTD_PCTL_R |= 0x11;
+		break;
+
+	case UART_3:
+		//enable GPIO clk
+		SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R2;
+		GPIO_PORTC_AFSEL_R |= GPIO_PC76_M;
+		GPIO_PORTC_DEN_R |= GPIO_PC76_M;
+		GPIO_PORTC_PCTL_R |= 0x11;
+		break;
+
+	case UART_4:
+
+		//enable GPIO clk
+		SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R2;
+		GPIO_PORTC_AFSEL_R |= GPIO_PC54_M;
+		GPIO_PORTC_DEN_R |= GPIO_PC54_M;
+		GPIO_PORTC_PCTL_R |= 0x11;
+		break;
+
+	case UART_5:
+
+		//enable GPIO clk
+		SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R4;
+		GPIO_PORTE_AFSEL_R |= GPIO_PE54_M;
+		GPIO_PORTE_DEN_R |= GPIO_PE54_M;
+		GPIO_PORTE_PCTL_R |= 0x11;
+		break;
+
+	case UART_6:
+	
+		//enable GPIO clk
+		SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R3;
+		GPIO_PORTD_AFSEL_R |= GPIO_PD54_M;
+		GPIO_PORTD_DEN_R |= GPIO_PD54_M;
+		GPIO_PORTD_PCTL_R |= 0x11;
+		break;
+
+	case UART_7:
+	
+
+		//enable GPIO clk
+		SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R4;
+		GPIO_PORTE_AFSEL_R |= GPIO_PE10_M;
+		GPIO_PORTE_DEN_R |= GPIO_PE10_M;
+		GPIO_PORTE_PCTL_R |= 0x11;
+		break;
+	}
+
 }
 
 
